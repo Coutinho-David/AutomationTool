4af84ce1aeeb0332754597bfac63ee5184dc06fb
cdm22b <davidcoutinho555@gmail.com>
2025-11-19 13:43:14 +0000

    NEW: Subdomain Takeovers, GoWitness, more tool usage for recon

--- automation.sh
+++ automation.sh
@@ -1,27 +1,15 @@
-#!/usr/bin/bash
 
 show_help() {
     echo "Usage: ./automation.sh [OPTIONS] domain"
     echo ""
     echo "Options:"
-    echo "  -h                Show this help message and exit"
-    echo "  -r      Recon     Do Recon on the target domain"
-    echo "  -f      FUZZ      Use FFUF to fuzz domain"
+    echo "  -h               Show this help message and exit"
+    echo "  -r  <domain>     Recon     Perform recon on the target domain"
+    echo "  -f  <domain>     FUZZ      Use FFUF to fuzz domain"
     exit 0
 }
 
-echo "
-________________________________________________________________________
-   ______ ____   __  ___
-  / ____// __ \ /  |/  /
- / /    / / / // /|_/ /      Pentesting Automation Tool by (C)DM
-/ /___ / /_/ // /  / /  
-\____//_____//_/  /_/   
-
-________________________________________________________________________"
-                        
-                      
-sleep 2
+echo "Starting"
 
 recon=false
 fuzz=false
@@ -61,13 +49,15 @@ fi
 
 if [ "$recon" = true ]; then
     echo "Performing Recon on $domain"
+
     sleep 1.5
+
     echo "----------------------------------------------------------------
                                     WHOIS"
-    whois -H $domain
+    whois $domain
 
     echo "-----------------------------------------------------------------
-                                    HOST " 
+                                    HOST "
     host $domain
 
     echo "----------------------------------------------------------------------"
@@ -78,62 +68,59 @@ if [ "$recon" = true ]; then
     echo "----------------------------------------------------------------------"
     echo "SUBLISTER SCAN"
 
-    sublist3r -d $domain -o "sublist3rScan.txt"
-    
+    python3 -m sublist3r -d $domain -o "sublist3rScan.txt"
 
-    httpx-toolkit -l "subfinderScan.txt" -sc -ip -td -server --follow-redirects -title -o "HttpxScanFinder.txt"
-    httpx-toolkit -l "sublist3rScan.txt" -sc -ip -td -server --follow-redirects -title -o "HttpxScanList3r.txt"
+    echo "----------------------------------------------------------------------"
+    echo "ASSET FINDER SCAN"
 
-    cat "HttpxScanFinder.txt" "HttpxScanList3r.txt" | sort | uniq > "final.txt"
+    ~/go/bin/assetfinder $domain -subs-only > "assetfinderScan.txt"
 
-    echo "--------------------------------------------------------------------------------------------------~"
-    echo "Combined and deduplicated domains saved to final.txt"
+    echo "----------------------------------------------------------------------"
+    echo "crt.sh SCAN"
 
+    curl "https://crt.sh?q=$domain&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > crt.txt
+
+    cat "crt.txt" "subfinderScan.txt" "sublist3rScan.txt" "assetfinderScan.txt" | sort | uniq > "allDomains.txt"
+
+    ~/go/bin/httpx -l "allDomains.txt" -sc -ip -td -server --follow-redirects -title -o "HttpxAllDomains.txt"
+    ~/go/bin/gowitness scan file -f "allDomains.txt"
 
     read -p "Do you wish to delete previous scan files? [Except for final](Y/n): " choice
-
     if [[ "$choice" == "Y" || "$choice" == "y" ]]; then
-        rm -f subfinderScan.txt sublist3rScan.txt HttpxScanFinder.txt HttpxScanList3r.txt
+        rm -f subfinderScan.txt sublist3rScan.txt assetfinderScan.txt
         echo "Deleted previous scan files..."
     else
         echo "Previous scan files were not deleted."
     fi
-
-    echo "You can GREP final.txt for status codes: 
-
-        1xx informational response
-        2xx success
-        3xx redirection
-        4xx client errors
-
-        200 : OK
-        204 : NO CONTENT
-        301 : MOVED PERMANENTLY 
-        401 : UNAUTHORIZED
-        403 : FORBIDDEN
-        404 : NOT FOUND
-        405 : METHOD NOT ALLOWED 
-
-        etc... visit https://en.wikipedia.org/wiki/List_of_HTTP_status_codes for more info"
 fi
 
 
-if [[ "$fuzz" = true ]]; then 
+    echo "----------------------------------------------------------------------"
+    echo "subzy SCAN"
+
+    ~/go/bin/subzy run --targets "allDomains.txt" --output "subzy.txt"
+
+    echo "----------------------------------------------------------------------"
+    echo "nuclei SCAN for subdomain takeover"
+
+    nuclei -list "allDomains.txt" -t /Users/cdm/xSTF/tools/nuclei-templates/nuclei-templates/http/takeovers/
+
+if [[ "$fuzz" = true ]]; then
     echo "Running FFUF fuzzing on $domain..."
     sleep 1.5
     read -p "Fuzzing for directories, subdomains, files or extensions? (dir/sub/fil/ext) " choice
     if [[ "$choice" = "dir" ]]; then
-        ffuf -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt:FUZZ -u "https://$domain/FUZZ"
+        ffuf -w /Users/cdm/xSTF/tools/SecLists/Discovery/Web-Content/directory-list-2.3-medium.txt:FUZZ -u "https://$domain/FUZZ"
     elif [[ "$choice" = "sub" ]]; then
-        ffuf -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt:FUZZ -u "https://FUZZ.$domain" -fc 404,403
+        ffuf -w /Users/cdm/xSTF/tools/SecLists/Discovery/DNS/subdomains-top1million-110000.txt:FUZZ -u "https://FUZZ.$domain" -fc 404,403
     elif [[ "$choice" = "fil" ]]; then
         read -p "What are the file extensions eg: php, aspx, html, etc...? IF UNKNOWN RUN script with ext" ext
-        ffuf -w /usr/share/seclists/Discovery/DNS/namelist.txt:FUZZ -u "https://$domain/FUZZ.$ext"
+        ffuf -w /Users/cdm/xSTF/tools/SecLists/Discovery/DNS/namelist.txt:FUZZ -u "https://$domain/FUZZ.$ext"
     elif [[ "$choice" = "ext" ]]; then
-        ffuf -w /usr/share/seclists/Discovery/Web-Content/web-extensions-big.txt:FUZZ -u "https://$domain/indexFUZZ"
-    else 
+        ffuf -w /Users/cdm/xSTF/tools/SecLists/Discovery/Web-Content/web-extensions-big.txt:FUZZ -u "https://$domain/indexFUZZ"
+    else
         echo "No option selected"
-        ./automation.sh -h 
+        ./automation.sh -h
         exit 1
     fi
 fi
